4d47931b6ed64f6dd961c2b4903fde30a7739eb5
RashidaAvshar <rashidaavshar@gmail.com>
2023-02-28 13:44:31 +0400

    all finish

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import {useState} from "react";
 import Button from "./Button";
 
 const Form = () =>{
+
+    const [userName, setUserName] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleChangeUsername = (e) => {
+        setUserName(e.target.value);
+    };
+    const handleChangePassword = (e) => {
+        setPassword(e.target.value);
+    };
+    const senData = ()=>{
+        const data = {
+            userName,
+            password
+        }
+        console.log(data)
+    }
+
     return <div className="form-box">
         <h1>LOGIN</h1>
-        <input type="text" />
-        <input type="password" />
-        <Button title="login" />
+        <input type="text" placeholder="name"  onChange={handleChangeUsername} />
+        <input type="password" placeholder="password" onChange={handleChangePassword}/>
+        <Button title="login" onClick={senData}/>
 
     </div>
 }
